68d41ff8fa3897359d415a72a2eb90d9400c0a48
Grzegorz Kuczera <grzegorzkuczera@gmial.com>
2019-07-30 23:38:05 +0200

    Start crerate security system

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default_index")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
